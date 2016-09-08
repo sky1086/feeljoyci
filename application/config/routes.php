d80b9c115f548cc543cb50c35645bbb6690e2475
sky1086 <skya.1086@gmail.com>
2016-09-08 08:22:16 +0530

    changed home

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
-$route['default_controller'] = 'user/listeners';
+$route['default_controller'] = 'user/topics';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
