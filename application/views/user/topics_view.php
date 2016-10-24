2431ab04af01abafc3ee6ac652aa54eb1689349d
sky1086 <skya.1086@gmail.com>
2016-10-24 23:11:41 +0530

    push notification

--- application/views/user/topics_view.php
+++ application/views/user/topics_view.php
@@ -124,6 +124,18 @@
     border-bottom: 1px solid #acacac;
 }
     </style>
+    <script type="text/javascript">
+    (function(p,u,s,h){
+        p._pcq=p._pcq||[];
+        p._pcq.push(['_currentTime',Date.now()]);
+        s=u.createElement('script');
+        s.type='text/javascript';
+        s.async=true;
+        s.src='https://cdn.pushcrew.com/js/2af3528e08af3f47d8eedf0b5be2ac28.js';
+        h=u.getElementsByTagName('script')[0];
+        h.parentNode.insertBefore(s,h);
+    })(window,document);
+</script>
 </head>
 
 <body>
