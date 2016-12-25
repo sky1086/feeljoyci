a64bdabb0a77405fc97f3ed580fe850fe30a1eac
sanjeevthb <sanjeev@thb.co.in>
2016-12-25 21:39:17 +0530

    new api for listener list

---
+++ application/controllers/apis/User.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User extends CI_Controller{
+   
+    function __construct(){
+        parent::__construct();
+    	//$this->authentication->isLoggedIn();  
+		$this->load->model(array('admin/question_model', 'admin/category_model', 'user/listeners_model'));
+	    }
+
+	    public function listeners(){
+	    	$data = $this->listeners_model->getListeners();
+	    	 
+	    	/*$isSpamUser = $this->chat_model->isSpamUser($this->session->userdata('userid'));//var_dump($isSpamUser);exit;
+	    	if ($isSpamUser){
+	    		$data['isSpamUser'] = $isSpamUser;
+	    	}*/
+	    	$result = [];
+	    	foreach ($data as $row){
+	    		$resultRow = [];
+	    		
+	    		$resultRow['id'] = $row['id'];
+	    		$resultRow['name'] = $row['name'];
+	    		$resultRow['qualification'] = $row['qualification'];
+	    		$resultRow['profile_img'] = base_url().'pics_listener/'.$row['profile_img'];
+	    		$resultRow['link'] = base_url().'user/chat/index/'.$row['id'];	    		
+	    		$resultRow['priority'] = $row['priority'];
+	    		$result[] = $resultRow;
+	    	}
+	    	echo json_encode($result);	
+	    }
+}
+?>
