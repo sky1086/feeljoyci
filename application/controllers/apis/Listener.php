00ca91e5790c717399630bc92364acc385ad7535
sky1086 <skya.1086@gmail.com>
2017-01-24 10:49:15 +0530

    minor bug fixe

--- application/controllers/apis/Listener.php
+++ application/controllers/apis/Listener.php
@@ -26,8 +26,26 @@ class Listener extends CI_Controller{
 	    		exit();
 	    	}
 	    	
-	    	$contactedUsers = $this->chat_model->getContactedUsers($this->session->userdata('userid'));	    	
-	    	echo json_encode($contactedUsers);
+	    	$contactedUsers = $this->chat_model->getContactedUsers($this->session->userdata('userid'));
+	    	if(!count($contactedUsers)){
+	    		echo json_encode(['error'=> true, 'result'=>null]);
+	    	}
+	    	
+	    	$result = [];
+	    	foreach ($contactedUsers as $user){
+	    		$user = $user[0];
+	    		$row = [
+	    				'userid'=> $user['userid'],
+	    				'user_type'=> $user['user_type'],
+	    				'age'=> $user['age'],
+	    				'contact_name'=> $user['contact_name'],
+	    				'profile_img'=> $user['profile_img'],
+	    				'gender'=> $user['gender'],
+	    				'chat_link'=> base_url().'listener/chat/index/'.$user['userid']
+	    		];
+	    		$result[] = $row;
+	    	}
+	    	echo json_encode(['error'=> false, 'result'=>$result]);
 	    	
 	    	//echo json_encode($result);	
 	    }
