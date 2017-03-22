a8e825a8f33f02af9f80069386120967de680dcf
sky1086 <skya.1086@gmail.com>
2017-03-23 00:00:31 +0530

    allowed cross origin

--- application/controllers/crons/Pushnotification.php
+++ application/controllers/crons/Pushnotification.php
@@ -8,6 +8,15 @@ class Pushnotification extends CI_Controller{
         $this->load->model(array(
         		'chat/chat_model', 'notification_model', 'user_model'
         ));
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+        	// Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        	// you want to allow, and if so:
+        	header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        	header('Access-Control-Allow-Credentials: true');
+        	header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }else{
+        	header('Access-Control-Allow-Origin: *'); //need to remove after developement done
+        }
     }
 
     public function index(){
